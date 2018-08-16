18a2a1a86deaaddc9195ff8c2e60f2708b321436
Derek Smith, david zhu and Kenneth Du <kenneth@>
2018-08-16 09:15:34 -0700

    add initial files.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  $("form#formOne").submit(function(event){
+  event.preventDefault();
+  var result = ("");
+  $("#result").text("#transNumber");
+
+  });
+
+
+
+
+});
